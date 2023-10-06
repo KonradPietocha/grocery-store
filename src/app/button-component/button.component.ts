c33bacc27bd84e2c8e1a8ad4d4849e1b2d182630
KonradPietocha <konrad.pietocha@gazeta.pl>
2023-10-06 12:10:36 +0200

    Corrected some styles

--- src/app/button-component/button.component.ts
+++ src/app/button-component/button.component.ts
@@ -6,6 +6,5 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./button.component.css']
 })
 export class ButtonComponent {
-  @Input() title: string;
-  @Input() isWarning: boolean;
+  @Input() type?: string;
 }
