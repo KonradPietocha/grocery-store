08c11ce624793235c557c59fba146817c4c20e5a
KonradPietocha <konrad.pietocha@gazeta.pl>
2023-10-06 10:32:18 +0200

    Messages corrected. Mobile icons added

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,24 +3,24 @@ import { Injectable } from '@angular/core';
 import { PRODUCTS } from './mock-products';
 import { Product } from './product';
 import { MessagesService } from './messages.service';
-
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
+import { map, tap } from 'rxjs/operators';
 
 @Injectable()
 export class ProductService {
   getDate: string = new Date().toLocaleString('en-GB');
 
+  constructor(private messagesService: MessagesService) { }
+
   getProducts(): Observable<Product[]> {
-    this.messagesService.add(`${this.getDate}: Your favorite products have been downloaded`);
     return of(PRODUCTS);
   }
 
   getProduct(id: number): Observable<Product> {
-    this.messagesService.add(`${this.getDate}: Selected product number: ${id}`);
-    return of(PRODUCTS.find(product => product.id === id));
+    return this.getProducts().pipe(
+      map((products: Product[]) => products.find(product => product.id === id)),
+      tap((product: Product) => this.messagesService.add(`${this.getDate}: Selected product: #${ product.id }-${ product.name }`))
+    )
   }
-
-  constructor(private messagesService: MessagesService) { }
-
 }
