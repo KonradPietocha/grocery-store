ac863da4873db02ccfbf028e118295b15a1ace2c
KonradPietocha <konrad.pietocha@gazeta.pl>
2023-10-08 07:54:12 +0200

    Corrected img src

--- src/app/mock-products.ts
+++ src/app/mock-products.ts
@@ -6,69 +6,69 @@ export const PRODUCTS: Product[] = [
         name: 'sausage',
         price: 25,
         quantity: 1,
-        imgSrc: '../../assets/images/sausage.jpg'
+        fileName: 'sausage.jpg'
     },
     {
         id: 2,
         name: 'carrots',
         price: 2,
         quantity: 1,
-        imgSrc: '../../assets/images/carrots.jpg'
+        fileName: 'carrots.jpg'
     },
     {
         id: 3,
         name: 'tomatoes',
         price: 4,
         quantity: 1,
-        imgSrc: '../../assets/images/tomatoes.jpg'
+        fileName: 'tomatoes.jpg'
     },
     {
         id: 4,
         name: 'cooking oil',
         price: 7,
         quantity: 1,
-        imgSrc: '../../assets/images/cooking-oil.jpg'
+        fileName: 'cooking-oil.jpg'
     },
     {
         id: 5,
         name: 'baking powder',
         price: 1,
         quantity: 1,
-        imgSrc: '../../assets/images/baking-powder.jpg'
+        fileName: 'baking-powder.jpg'
     },
     {
         id: 6,
         name: 'coffee',
         price: 20,
         quantity: 1,
-        imgSrc: '../../assets/images/coffee.jpg'
+        fileName: 'coffee.jpg'
     },
     {
         id: 7,
         name: 'salt',
         price: 1,
         quantity: 1,
-        imgSrc: '../../assets/images/salt.jpg'
+        fileName: 'salt.jpg'
     },
     {
         id: 8,
         name: 'beef cuts',
         price: 40,
         quantity: 1,
-        imgSrc: '../../assets/images/beef-cuts.jpg'
+        fileName: 'beef-cuts.jpg'
     },
     {
         id: 9,
         name: 'frozen pizza',
         price: 7,
         quantity: 1,
-        imgSrc: '../../assets/images/frozen-pizza.jpg'
+        fileName: 'frozen-pizza.jpg'
     },
     {
         id: 10,
         name: 'cheese',
         price: 13,
         quantity: 1,
-        imgSrc: '../../assets/images/cheese.jpg'
+        fileName: 'cheese.jpg'
     }
 ];
